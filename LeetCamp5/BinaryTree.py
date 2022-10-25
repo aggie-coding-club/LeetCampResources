7dbf4ec81e977f3f6901e9aead63a7953b1fe386
Andrew Fennell <andrew.fennell1999@me.com>
2022-10-25 11:49:53 -0500

    First commit

---
+++ LeetCamp5/BinaryTree.py
@@ -0,0 +1,27 @@
+class TreeNode:
+    def __init__(self, val, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+###############################################
+####             Your Function             ####
+###############################################
+def yourFunction(root: TreeNode):
+    pass
+
+# This is how your tree is built from input
+def build_tree(nodes, f):
+    val = next(nodes)
+    if val == 'x': return None
+    left = build_tree(nodes, f)
+    right = build_tree(nodes, f)
+    return TreeNode(f(val), left, right)
+
+if __name__ == '__main__':
+    # Example Input: 5 4 3 x x 8 x x 6 x x
+    # Reference https://tx.ag/LeetCamp5Slides
+    test_case = input() # You can hard code this or make many tests
+    #test_case = "5 4 3 x x 8 x x 6 x x"
+    root = build_tree(iter(test_case.split()), int) # Change int if different type
+    print(yourFunction(root)) # EDIT THIS TO BE YOUR FUNCTION NAME
